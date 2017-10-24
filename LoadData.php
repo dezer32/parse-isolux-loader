e416518e7979ec5347a30f8d8d73ba43d36294cb
Dezer32 <mr.vladkuz@gmail.com>
2017-10-24 15:39:46 +0300

    init

---
+++ LoadData.php
@@ -0,0 +1,8 @@
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+require __DIR__ . "/SectionSettings.php";
+
+foreach ($arSectionsUrl as $section => $children) {
+
+}
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_after.php");
